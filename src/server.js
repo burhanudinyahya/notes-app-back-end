e334585967296bdd6b95ec92f14d6d378e7976d9
Burhanudin Yahya <bor.yahya@gmail.com>
2022-10-12 22:33:42 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
- 
+const NoteValidator = require('./validator/notes');
+
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
@@ -13,14 +14,15 @@ const init = async () => {
       },
     },
   });
- 
+
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
+      validator: NoteValidator,
     },
   });
- 
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
